8f4282609f3c40eadda739b98ed36baeb12a84c5
chenhao <chenhaoitcode@163.com>
2020-12-14 09:45:00 +0800

    固守

---
+++ src/main/java/com/goodsogood/GushouApplication.java
@@ -0,0 +1,13 @@
+package com.goodsogood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GushouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GushouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goodsogood/gushou/GushouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodsogood.gushou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GushouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
