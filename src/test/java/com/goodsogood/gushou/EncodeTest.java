31fc2020c0a102ad09a83d423859a6c2f596bd13
chenhao <chenhaoitcode@163.com>
2020-12-16 17:41:29 +0800

    测试类

---
+++ src/test/java/com/goodsogood/gushou/EncodeTest.java
@@ -0,0 +1,25 @@
+package com.goodsogood.gushou;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+
+/**
+ * 作者：chenhao
+ * 日期：12/15/20 10:16 AM
+ **/
+public class EncodeTest {
+    @Test
+    public void test1(){
+        ByteBuffer encode = Charset.forName("UTF-8").encode("陈昊");
+        byte[] array = encode.array();
+        for (byte b : array) {
+            System.out.print(b);
+        }
+        //https://oauth.open.weidian.com/token?grant_type=client_credential&appkey=3703027904&secret=2be1fb189aa9123ed77ff9041a1a9494
+        //param={"page_size":"40","status":1,"orderby":"1","page_num":"1","update_start":"2012-11-12 16:36:08","update_end":"2019-11-12 16:36:08"}&public={"method":"vdian.item.list.get","format":"json","access_token":"c6b28ddee7069ba42eb998fc0abb05700009219503","version":"1.1"}
+        //c6b28ddee7069ba42eb998fc0abb05700009219503
+
+    }
+}
--- src/test/java/com/goodsogood/gushou/GushouApplicationTests.java
+++ src/test/java/com/goodsogood/gushou/GushouApplicationTests.java
@@ -1,13 +1,49 @@
 package com.goodsogood.gushou;
 
+import com.alibaba.fastjson.JSONObject;
+import com.goodsogood.GushouApplication;
+import com.goodsogood.config.VdianGetToken;
+import com.goodsogood.utils.VdianRestUtil;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import java.util.HashMap;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes= GushouApplication.class)
 class GushouApplicationTests {
+
+    @Autowired
+    private VdianRestUtil vdianRestUtil;
 
     @Test
     void contextLoads() {
+        String token = vdianRestUtil.getToken();
+        System.out.println(token);
+    }
+
+    @Test
+    void getRest(){
+        HashMap<String, Object> param = new HashMap<>();
+        HashMap<String, Object> paramPublic = new HashMap<>();
+//{"page_size":"40","status":1,"orderby":"1","page_num":"1","orderby":"5"}
+        param.put("page_size","5");
+        param.put("status",1);
+        param.put("orderby","1");
+        param.put("page_num","1");
+//{"method":"vdian.item.list.get","format":"json","access_token":"c6b28ddee7069ba42eb998fc0abb05700009219503","version":"1.1"}
+        paramPublic.put("method","vdian.item.list.get");
+        paramPublic.put("format","json");
+        paramPublic.put("access_token",VdianGetToken.token);
+        paramPublic.put("version","1.1");
+        JSONObject results = vdianRestUtil.getRestApi(param, paramPublic);
+        JSONObject result = results.getJSONObject("result");
+
+        System.out.println(result);
+
     }
 
 }
