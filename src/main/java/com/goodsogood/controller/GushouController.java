470c149ef0ad0e3eab007c99b6a2c610addb839e
yangf <1425189893@qq.com>
2020-12-17 19:41:11 +0800

    日志的加入

--- src/main/java/com/goodsogood/controller/GushouController.java
+++ src/main/java/com/goodsogood/controller/GushouController.java
@@ -3,6 +3,8 @@ package com.goodsogood.controller;
 import com.goodsogood.response.BaseResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -16,14 +18,14 @@ import org.springframework.web.client.RestTemplate;
 @CrossOrigin
 @RequestMapping("/rest/gushou")
 public class GushouController {
-
+    private final static Logger logger = LoggerFactory.getLogger(GushouController.class);
     @CrossOrigin
     @ApiOperation(value = "测试接口", notes = "测试接口")
     @ResponseBody
     @RequestMapping(value = "/refreshToken", method = RequestMethod.POST)
     public BaseResponse getToken() {
         try {
-
+            logger.info("正在获取Token信息中。。。。。。。");
             return BaseResponse.initSuccessBaseResponse("获取Token成功");
         } catch (Exception e) {
             return BaseResponse.initErrorBaseResponse("获取Token异常："+e.getMessage());
