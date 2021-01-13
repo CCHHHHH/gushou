a3ec502876ebfdab88db02c164c3bdbd1ec44005
chenhao <chenhaoitcode@163.com>
2021-01-13 15:35:32 +0800

    01.13

--- src/main/java/com/goodsogood/utils/GushouRestUtil.java
+++ src/main/java/com/goodsogood/utils/GushouRestUtil.java
@@ -2,17 +2,15 @@ package com.goodsogood.utils;
 
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.client.methods.HttpHead;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
-import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URLEncoder;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
@@ -46,7 +44,7 @@ public class GushouRestUtil {
             map.put("_hs", ser.get(0));
             map.put("access_key", access_key);
             String obj = JSONObject.toJSONString(map);
-            re = URLEncoder.encode(Base64.getEncoder().encodeToString(obj.getBytes("utf-8")), "utf-8");
+            re = Base64.getEncoder().encodeToString(obj.getBytes("utf-8"));
         } catch (Exception e) {
             log.error("数据加密错误", e);
             throw new RuntimeException("数据加密错误");
@@ -61,9 +59,9 @@ public class GushouRestUtil {
 
         HashMap<String, String> map1 = new HashMap<>();
         map1.put("data", ser.get(1));
+//        map1.put("data", "ynAbVi4jCVvmGF7AVvZfvFs3RULWL6%2BsPN6Lrs2BZHxqRUDiyQ572eZW8Nq%2BBre0IqXq%2Fo1%2FTUYHxeyqbJ9XdHTNnrC33CSGjN7wKvrpZZ5KdYmoLs95zvJ8gg0kWHZhE8yP5pGOOR%2BR6eR31fErV4H7Ih8O35o5Bxhaedpawh3Q1e%2BWcYoJUBsEgUPDnPxhlWXwzNqSgZuBMQacViHB3vcK72Z2OU6QHg39RZRhcoSA64HFtZyl1KA9KEuJH9DWFUozAAIQ3P3hVC2WUEQaFF9fRNgffUv4DEGDg%2F2RLStfuYacqwva6Dzj8TuxdEsLGQ12V1DUwbCDq9oclV41%2FvSb%2BooZ3oRyogmTnqp34%2BBHtd%2BFyE8u%2B8NtRYaVJIB74%2FveESIdN%2FVavdc2T4g5TVcUUZxOe2pDbgCwtLWRHRuYMvtM%2BcdeL%2FslGR8990GY3lknGu%2B2YAmGGu8EyhNP8p8B%2BCBO%2FY7u00s0EjLyP%2BQBXqsMQJZNcgQh0I2LobMiWB472GJofMGLsH9BWbw7wCmjFWKOvBpMEMOtXaZVLzOY9lHgUNkDpkz7duOwUiHs4tvwiNIX%2BbbL4ru9QHwav4v91PerH%2FuUgFeyH1p%2Fz%2FwaJSJ855autYncJlzcSUA3l4B6aucJswUE9lb9d57ETm5jqcp6iJdVZEtNerbom0MEemqfpzcyMw%3D%3D");
         String s = JSONObject.toJSONString(map1);
-//        String res = "{\"data\":\"" + ser.get(1) +"\"}";
-//        System.out.println("加密的消息体：" + res);
+
 
         String url1 = gushouApiUrl + url + "?_h=" + re + "&info=" + info;
         System.out.println(s);
